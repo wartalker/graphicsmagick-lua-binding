1797a41ba3da6b9bf578146e04601e80bff5b830
wartalker <wartalker@gmail.com>
2015-11-12 16:51:54 +0800

    rotate

--- image.lua
+++ image.lua
@@ -3,7 +3,7 @@
 local ffi = require "ffi"
 
 local _M = {}
-_M._VERSION = '0.4'
+_M._VERSION = '0.5'
 local mt = { __index = _M }
 
 ffi.cdef
@@ -63,8 +63,10 @@ MagickWand* DestroyMagickWand( MagickWand * );
 // Free resouse
 unsigned int MagickRelinquishMemory( void *resource );
 
-//
-char * MagickGetImageFormat( MagickWand *wand );
+// Desc
+unsigned char * MagickGetImageFormat( MagickWand *wand );
+unsigned char * MagickGetImageProfile( MagickWand *wand, const char *name, unsigned long *length );
+unsigned char * MagickDescribeImage( MagickWand *wand );
 
 // Read/Write
 MagickBooleanType MagickReadImageBlob( MagickWand*, const void*, const size_t );
@@ -92,6 +94,12 @@ unsigned int MagickAnnotateImage( MagickWand *wand, const DrawingWand *drawing_w
                                   const double x, const double y, const double angle,
                                   const char *text );
 
+// Rotate
+unsigned int MagickRotateImage( MagickWand *wand, const PixelWand *background,
+                                const double degrees );
+unsigned int MagickFlipImage( MagickWand *wand );
+unsigned int MagickFlopImage( MagickWand *wand );
+
 // *** Drawing Wand ***
 DrawingWand *MagickNewDrawingWand();
 void MagickDestroyDrawingWand( DrawingWand *drawing_wand );
@@ -117,6 +125,39 @@ unsigned int PixelSetColor( PixelWand *wand, const char *color );
 local libgm = ffi.load('libGraphicsMagickWand')
 libgm.InitializeMagick()
 
+local function new_pixel_wand()
+	local pwand = ffi.gc(libgm.NewPixelWand(), function(w)
+			libgm.DestroyPixelWand(w)
+	end)
+	return pwand
+end
+
+local function new_draw_wand()
+	local dwand = ffi.gc(libgm.MagickNewDrawingWand(), function(w)
+		libgm.MagickDestroyDrawingWand(w)
+	end)
+	return dwand
+end
+
+local function set_gravity(dwand, gravity)
+	local g = libgm[gravity .. 'Gravity']
+	if g == nil then
+		return 0
+	end
+   	return libgm.MagickDrawSetGravity(dwand, g)
+end
+
+local function ori(s)
+	local o = string.match(s, 'Orientation:%s*(%d)')
+	if o ~= nil then
+		local n = tonumber(o)
+		if 0 < n and n < 9 then
+			return n
+		end
+	end
+	return 0
+end
+
 function _M.new(self, img, t)
 	local mwand = ffi.gc(libgm.NewMagickWand(), function(w)
 		libgm.DestroyMagickWand(w)
@@ -140,17 +181,75 @@ function _M.height(self)
 	return libgm.MagickGetImageHeight(self._mwand)
 end
 
+function _M.profile(self, name)
+	local len = ffi.new('size_t[1]', 0)
+	local blob = libgm.MagickGetImageProfile(self._mwand, name, len)
+	if ffi.cast('void *', blob) > nil then
+		local s = ffi.string(blob, len[0])
+		libgm.MagickRelinquishMemory(blob)
+		return s
+	else
+		return nil
+	end
+end
+
+function _M.info(self)
+	local blob = libgm.MagickDescribeImage(self._mwand)
+	if ffi.cast('void *', blob) > nil then
+		local s = ffi.string(blob)
+		libgm.MagickRelinquishMemory(blob)
+		return s
+	else
+		return nil
+	end
+end
+
+function _M.rotate(self, degree)
+	local pwand = new_pixel_wand()
+	if pwand == nil then
+		return 0
+	end
+	return libgm.MagickRotateImage(self._mwand, pwand, degree)
+end
+
+function _M.flip(self)
+	return libgm.MagickFlipImage(self._mwand)
+end
+
+function _M.flop(self)
+	return libgm.MagickFlopImage(self._mwand)
+end
+
+function _M.jpeg_rotate(self)
+	local info = self:info()
+	local o = ori(info)
+	if     o == 1 then
+	elseif o == 2 then
+		self:flop()
+	elseif o == 3 then
+		self:rotate(180)
+	elseif o == 4 then
+		self:flip()
+	elseif o == 5 then
+		self:rotate(90)
+		self:flop()
+	elseif o == 6 then
+		self:rotate(90)
+	elseif o == 7 then
+		self:rotate(-90)
+		self:flop()
+	elseif o == 8 then
+		self:rotate(-90)
+	end
+end
+
 function _M.resize(self, w, h)
 	if h == nil or h == 0 then
-		local iw = self:width()
-		local ih = self:height()
-		h = w * ih / iw
+		h = w * self:height() / self:width()
 	end
 
 	if w == nil or w == 0 then
-		local iw = self:width()
-		local ih = self:height()
-		w = h * iw / ih
+		w = h * self:width() / self:height()
 	end
 
 	local filter = libgm['LanczosFilter']
@@ -163,11 +262,13 @@ end
 
 function _M.strip(self)
 	local t = ffi.string(libgm.MagickGetImageFormat(self._mwand))
-	if t ~= 'JPEG' then
-		return libgm.MagickStripImage(self._mwand)
-	else
-		return 1
-	end
+	return libgm.MagickStripImage(self._mwand)
+
+--	if t ~= 'JPEG' then
+--		return libgm.MagickStripImage(self._mwand)
+--	else
+--		return 1
+--	end
 end
 
 function _M.string(self)
@@ -186,20 +287,6 @@ function _M.save(self, path)
 	return libgm.MagickWriteImage(self._mwand, path)
 end
 
-local function new_pixel_wand()
-	local pwand = ffi.gc(libgm.NewPixelWand(), function(w)
-			libgm.DestroyPixelWand(w)
-	end)
-	return pwand
-end
-
-local function new_draw_wand()
-	local dwand = ffi.gc(libgm.MagickNewDrawingWand(), function(w)
-		libgm.MagickDestroyDrawingWand(w)
-	end)
-	return dwand
-end
-
 local function set_font(dwand, path, size, color)
 	if libgm.MagickDrawSetFont(dwand, path) == 0 then
 		return 0
@@ -215,14 +302,6 @@ local function set_font(dwand, path, size, color)
 	end
 
 	return libgm.MagickDrawSetFillColor(dwand, pwand)
-end
-
-local function set_gravity(dwand, gravity)
-	local g = libgm[gravity .. 'Gravity']
-	if g == nil then
-		return 0
-	end
-   	return libgm.MagickDrawSetGravity(dwand, g)
 end
 
 function _M.annote(self, path, size, color, pos, text)
